fab257ededdcb2bb4dc43faf59acbda07ee30625
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-14 17:18:03 +0600

    implement refresh

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+
+const App = () => {
+  const [Refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 14, item: 'Item 14' }]);
+    setRefreshing(false);
+  }
+  const [Items, setItems] = useState([
+    { key: 1, item: 'Item 1' },
+    { key: 2, item: 'Item 2' },
+    { key: 3, item: 'Item 3' },
+    { key: 4, item: 'Item 4' },
+    { key: 5, item: 'Item 5' },
+    { key: 6, item: 'Item 6' },
+    { key: 7, item: 'Item 7' },
+    { key: 8, item: 'Item 8' },
+    { key: 9, item: 'Item 9' },
+    { key: 10, item: 'Item 10' },
+    { key: 11, item: 'Item 11' },
+    { key: 12, item: 'Item 12' },
+    { key: 13, item: 'Item 13' },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView
+      style={styles.body}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+    >
+      {
+        Items.map((Object) => {
+          return (
+            <View style={styles.item} key={Object.key}>
+              <Text style={{ color: 'white', fontSize: 20, padding: 10 }}>{Object.item}</Text>
+            </View>
+          )
+        })
+      }
+    </ScrollView >
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black'
   },
-});
+  item: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
+  }
+})
+export default App;

d1757b7081b64238f2bfd06c871e66774956931e
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-15 22:41:53 +0600

    lern stacNav

--- App.js
+++ App.js
@@ -1,51 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
+const Stack = createStackNavigator();
+
+const ScreenA = ({ navigation }) => {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  }
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : 'goldenrod' })}
+      >
+        <Text style={styles.text}>Go to Screen B</Text>
+      </Pressable>
+    </View>
+  )
+}
+const ScreenB = ({ navigation }) => {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_A');
+  }
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen B</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : 'goldenrod' })}
+      >
+        <Text style={styles.text}>Go to Screen A</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 const App = () => {
-  const [Refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(true);
-    setItems([...Items, { key: 14, item: 'Item 14' }]);
-    setRefreshing(false);
-  }
-  const [Items, setItems] = useState([
-    { key: 1, item: 'Item 1' },
-    { key: 2, item: 'Item 2' },
-    { key: 3, item: 'Item 3' },
-    { key: 4, item: 'Item 4' },
-    { key: 5, item: 'Item 5' },
-    { key: 6, item: 'Item 6' },
-    { key: 7, item: 'Item 7' },
-    { key: 8, item: 'Item 8' },
-    { key: 9, item: 'Item 9' },
-    { key: 10, item: 'Item 10' },
-    { key: 11, item: 'Item 11' },
-    { key: 12, item: 'Item 12' },
-    { key: 13, item: 'Item 13' },
-  ]);
-
   return (
-    <ScrollView
-      style={styles.body}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          header: () => null
+        }}
+      >
+        <Stack.Screen
+          name="Screen_A"
+          component={ScreenA}
         />
-      }
-    >
-      {
-        Items.map((Object) => {
-          return (
-            <View style={styles.item} key={Object.key}>
-              <Text style={{ color: 'white', fontSize: 20, padding: 10 }}>{Object.item}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView >
+        <Stack.Screen
+          name="Screen_B"
+          component={ScreenB}
+        />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
   );
 };
 
@@ -53,14 +66,13 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
-  },
-  item: {
-    backgroundColor: 'red',
     justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10
+    alignItems: 'center'
+  },
+  text: {
+    margin: 10,
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 })
 export default App;

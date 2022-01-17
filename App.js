8aa2feb93fd7333fb0a109a0120c72fd1eb726c4
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-17 08:45:57 +0600

    done

--- App.js
+++ App.js
@@ -1,66 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 
 const App = () => {
-  const [Refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(true);
-    setItems([...Items, { key: 14, item: 'Item 14' }]);
-    setRefreshing(false);
+  const [text, onChangeText] = useState('');
+  const [number, onChangeNumber] = useState('');
+
+  const [textWarning, setTextWarning] = useState('');
+  const [numberWarning, setNumberWarning] = useState('');
+
+
+  const onPressFunction = () => {
+    if (text.length === 0) {
+      setTextWarning('please enter your id')
+    }
+    if ((text.length > 0 && text.length < 9) || (text.length > 9)) {
+      setTextWarning('id must be 8 char')
+    }
+
+    if (number.length === 0) {
+      setNumberWarning('please enter your number')
+    }
+    if ((number.length > 0 && number.length < 11) || (number.length > 11)) {
+      setNumberWarning('number must be 8 char')
+      if ((number.startsWith('01') != true)) {
+        setNumberWarning('valid input num')
+      }
+    }
+
   }
-  const [Items, setItems] = useState([
-    { key: 1, item: 'Item 1' },
-    { key: 2, item: 'Item 2' },
-    { key: 3, item: 'Item 3' },
-    { key: 4, item: 'Item 4' },
-    { key: 5, item: 'Item 5' },
-    { key: 6, item: 'Item 6' },
-    { key: 7, item: 'Item 7' },
-    { key: 8, item: 'Item 8' },
-    { key: 9, item: 'Item 9' },
-    { key: 10, item: 'Item 10' },
-    { key: 11, item: 'Item 11' },
-    { key: 12, item: 'Item 12' },
-    { key: 13, item: 'Item 13' },
-  ]);
+
 
   return (
-    <ScrollView
-      style={styles.body}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
         />
-      }
-    >
-      {
-        Items.map((Object) => {
-          return (
-            <View style={styles.item} key={Object.key}>
-              <Text style={{ color: 'white', fontSize: 20, padding: 10 }}>{Object.item}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView >
+        <Text>{textWarning}</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+        <Text>{numberWarning}</Text>
+      </View>
+      <Pressable onPress={onPressFunction}>
+        <Text>I'm pressable!</Text>
+      </Pressable>
+
+    </SafeAreaView>
+
   );
 };
 
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 10,
   },
-  item: {
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10
-  }
-})
+});
 export default App;
